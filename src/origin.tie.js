02e6700842bf925e98f7c31c6722b704a797cd64
Lemony Andrew <lemonyandrew@gmail.com>
2016-01-13 19:46:28 +0000

    Took ids out of elTie-list. Added many function comment definitions. Decided how the overall link/tying process will be executed.  doesn't automatically turn on syncing. TiedProperties have more information needed for syncing. Started implementing the direct synchronizing function, and started implementing the parsing function.

--- src/origin.tie.js
+++ src/origin.tie.js
@@ -1,7 +1,4 @@
 /* global Origin */
-/// Standardize an observer namespace
-window.MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
-
 Origin.Tie = new(function OriginTie(){
     "use strict";
     
@@ -10,28 +7,50 @@ Origin.Tie = new(function OriginTie(){
     */
     var tieList = {
         // "0": OriginElTile ...
-        ids: 0
     };
+    var ties = 0;
     
     
-    var updateInterval;
+    /**
+        @summary: The main loop for keeping JavaScript and DOM synced.
+    */
     function processChanges(){
-        
+        for( var index in tieList ){
+            tieList[ index ]._synchronize();
+        }
     };
     
-    this.turnOff = function stopTying(){
-        window.clearInterval( updateInterval );
-    };
-    
+    // how fast (ms) to update the screen 
     var tieWait = 10;
+    // the actual interval reference
+    var updateInterval = undefined;
+    
+    /**
+        @summary: Turns on updates between DOM and JavaScript handled by Origin. 
+    */
     this.turnOn = function startTying(){
         updateInterval = window.setInterval( processChanges, tieWait );
     };
     
+    /**
+        @summary: turns off any updating between dom and javascript handled by
+            Origin. 
+    */
+    this.turnOff = function stopTying(){
+        window.clearInterval( updateInterval );
+        updateInterval = undefined;
+    };
+    
+    /**
+        @summary: Changes the interval rate at which updates occur.
+        @argument "speed": milliseconds for interval calls.
+    */
     this.changeUpdateSpeed = function tieSpeed( speed ){
         tieWait = speed;
-        this.turnOff();
-        this.turnOn();
+        if( updateInterval ){
+            this.turnOff();
+            this.turnOn();
+        }
     };
     
     
@@ -44,10 +63,9 @@ Origin.Tie = new(function OriginTie(){
     */
     function OriginElTie(elRef) {
         this.el = elRef;
-    }
+    };
     OriginElTie.prototype = new(function OriginDomPrototype() {
         // A list of properties being synced on our node
-        // { "htmlProperty": { base: baseObj, baseProperty: -} }
         this.tiedProps = {};
     
         /**
@@ -57,10 +75,12 @@ Origin.Tie = new(function OriginTie(){
             @argument "baseObj": The base origin object 
             @argument "baseProp": The base origin object's property tied with
         */
-        function defineTiedProperties(htmlProp, baseObj, baseProp) {
+        function defineTiedProperty(htmlProp, baseObj, baseProp, canWrite) {
             this.propertySyncs[htmlProp] = {
                 baseObj: baseObj,
-                baseProperty: baseProp
+                baseProperty: baseProp,
+                canWrite: canWrite,
+                previousValue: null
             };
         };
     
@@ -69,20 +89,51 @@ Origin.Tie = new(function OriginTie(){
             @argument "baseObj": The base origin object 
             @argument "baseProp": The base origin object's property tied with
         */
-        this.tieProperty = function tieProperty(htmlProp, baseObj, baseProp) {
+        this.tieProperty = function tieProperty(htmlProp, baseObj, baseProp, write) {
             if (htmlProp in this.tiedProps) {
                 var err = new Error(htmlProp + " is already defined on element: " + this.el);
                 err.id = "OGTiePropOver";
                 throw err;
             }
-        }
-    
+            defineTiedProperty( htmlProp, baseObj, baseProp, write );
+        };
+        
+        /**
+            @summary: the main logic behind synchronizing elements.
+        */
+        this._synchronize = function synchronize(){
+            for( var prop in this.tiedProps ){
+                var propData = this.tiedProps[prop];
+                var curVal = this.el[prop] 
+            }
+        };
+        
+        /**
+            @summary: Function that can be called to initiate the syncrhonization
+                of the DOM and Origin. You can either do a direct parse or a deep
+                parse. 
+                Parsing in this context means scanning values to sync properties 
+                together.
+            @argument "parseChildren": Boolean. If true, parses ALL child nodes.
+        */
+        this.parse = function parseDom(parseChildren){
+            // We combine attributes with innerHTML to support direct text insertions
+            var props = this.el.attributes;
+            props[props.length++] = { name:"innerHTML"  };
+            
+            for( var i = 0, l = props.length; i < l; i ++){
+                var prop = props[i].name;
+                console.log(prop, this.el[prop]);
+            }
+        };
+        
     })();
     
     /**
-        @summary: Function that will take dom instance, parse it, 
+        @summary: A function that gets/creates the tie between a dom element and
+        
     */
-    function OriginDom(el) {
+    this.get = function OriginDom(el) {
         // we only want to apply listening to html elements. 
         if (el instanceof HTMLElement === false) {
             var err = new Error("Only HTML Elements are excepted.");
@@ -92,8 +143,8 @@ Origin.Tie = new(function OriginTie(){
         
         // check if element has an origin reference, else make one. 
         if (el.dataset.originId in tieList === false) {
-            el.dataset.originId = tieList.ids++;
-            return (tieList[tieList.ids - 1] = new OriginElTie(el))
+            el.dataset.originId = ties++;
+            return (tieList[ties - 1] = new OriginElTie(el))
         }
         
         // return the existing element reference 
